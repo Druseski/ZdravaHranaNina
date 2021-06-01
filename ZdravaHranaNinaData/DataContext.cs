cd12749c61abf386afa7f95b0fd67cccfa14c18b
Druseski <76123000+Druseski@users.noreply.github.com>
2021-06-01 13:09:48 +0200

    Add project files.

---
+++ ZdravaHranaNinaData/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZdravaHranaNinaData
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+    }
+}
